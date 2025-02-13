d9ba8e6829dedb076f66b9e147a3e910fcb4ec29
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-02-13 15:15:01 -0800

    added routes

--- app.js
+++ app.js
@@ -11,6 +11,23 @@ app.set("view engine", "ejs");
 
 const PORT = 3000;
 
+
+app.get('/', (req, res) => {
+
+  res.render('home');
+});
+
+
+app.post('/thankyou', (req, res) => {
+  
+  res.render('confirm', { order });
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`.bgYellow);
 });
+
+
+

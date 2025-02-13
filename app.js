03a54b984e4a0fe9ff4d4f5830ff341f34a8ce2e
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-02-13 15:36:03 -0800

    Finished challenge

--- app.js
+++ app.js
@@ -11,23 +11,31 @@ app.set("view engine", "ejs");
 
 const PORT = 3000;
 
+const orders = [];
 
-app.get('/', (req, res) => {
-
-  res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
+app.post("/thankyou", (req, res) => {
+  console.log(req.body);
+  const order = {
+    name: req.body.name,
+    email: req.body.email,
+    flavors: req.body.flavors,
+    cone: req.body.cone,
+    toppings: req.body.toppings,
+    comment: req.body.comment,
+  };
 
-app.post('/thankyou', (req, res) => {
-  console.log(req.body)
-  res.render('confirm');
+  orders.push(order);
+  res.render("confirm", { order });
 });
 
-
+app.get("/admin", (req, res) => {
+  res.render("admin", { orders });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`.bgYellow);
 });
-
-
-

992c87b15b6ace290d78796efad535378f54561b
Bolshialex <145608606+Bolshialex@users.noreply.github.com>
2025-02-13 15:05:23 -0800

    Add server details

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 import express from "express";
+import colors from "colors";
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`.bgYellow);
+});
